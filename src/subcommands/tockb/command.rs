89aa48a9854d66039eb715579650d7a34d22f307
jacobdenver007 <jacobdenver@163.com>
2020-09-16 19:31:00 +0800

    feat(bonding): add bonding command

--- src/subcommands/tockb/command.rs
+++ src/subcommands/tockb/command.rs
@@ -155,6 +155,23 @@ impl<'a> ToCkbSubCommand<'a> {
                     .arg(Arg::from("-p --pledge=[pledge] 'pledge'"))
                     .arg(Arg::from("-l --lot_size=[lot_size] 'lot_size'"))
                     .arg(Arg::from("-k --kind=[kind] 'kind'")),
+                App::new("bonding")
+                    .about("bonding signer")
+                    .arg(arg::privkey_path().required_unless(arg::from_account().get_name()))
+                    .arg(
+                        arg::from_account()
+                            .required_unless(arg::privkey_path().get_name())
+                            .conflicts_with(arg::privkey_path().get_name()),
+                    )
+                    .arg(arg::from_locked_address())
+                    .arg(arg::tx_fee().required(true))
+                    .arg(arg::derive_receiving_address_length())
+                    .arg(
+                        arg::derive_change_address().conflicts_with(arg::privkey_path().get_name()),
+                    )
+                    .arg(Arg::from("--original_tx_hash=[original_tx_hash] 'original_tx_hash'"))
+                    .arg(Arg::from("--original_tx_output_index=[original_tx_output_index] 'original_tx_output_index'"))
+                    .arg(Arg::from("-k --kind=[kind] 'kind'")),
             ])
     }
 
@@ -547,6 +564,361 @@ impl<'a> ToCkbSubCommand<'a> {
         Ok(tx)
     }
 
+    pub fn bonding(
+        &mut self,
+        args: BondingArgs,
+        skip_check: bool,
+        settings: Settings,
+    ) -> Result<TransactionView, String> {
+        let BondingArgs {
+            privkey_path,
+            from_account,
+            from_locked_address,
+            password,
+            derive_receiving_address_length,
+            derive_change_address,
+            tx_fee,
+
+            kind,
+            original_tx_hash,
+            original_tx_output_index,
+        } = args;
+
+        let network_type = get_network_type(self.rpc_client)?;
+        let from_privkey: Option<PrivkeyWrapper> = privkey_path
+            .map(|input| PrivkeyPathParser.parse(&input))
+            .transpose()?;
+        let from_account: Option<H160> = from_account
+            .map(|input| {
+                FixedHashParser::<H160>::default()
+                    .parse(&input)
+                    .or_else(|err| {
+                        let result: Result<Address, String> = AddressParser::new_sighash()
+                            .set_network(network_type)
+                            .parse(&input);
+                        result
+                            .map(|address| H160::from_slice(&address.payload().args()).unwrap())
+                            .map_err(|_| err)
+                    })
+            })
+            .transpose()?;
+        let from_locked_address: Option<Address> = from_locked_address
+            .map(|input| {
+                AddressParser::default()
+                    .set_network(network_type)
+                    .set_full_type(MULTISIG_TYPE_HASH.clone())
+                    .parse(&input)
+            })
+            .transpose()?;
+
+        /// assume price is 100_000 for now.
+        let price = 100_000;
+        let additional_to_capacity = 2 * 200 * 100_000_000;
+        let tx_fee: u64 = CapacityParser.parse(&tx_fee)?.into();
+        let receiving_address_length: u32 = derive_receiving_address_length
+            .map(|input| FromStrParser::<u32>::default().parse(&input))
+            .transpose()?
+            .unwrap_or(1000);
+        let last_change_address_opt: Option<Address> = derive_change_address
+            .map(|input| {
+                AddressParser::default()
+                    .set_network(network_type)
+                    .parse(&input)
+            })
+            .transpose()?;
+
+        let (from_address_payload, password) = if let Some(from_privkey) = from_privkey.as_ref() {
+            let from_pubkey = secp256k1::PublicKey::from_secret_key(&SECP256K1, from_privkey);
+            (AddressPayload::from_pubkey(&from_pubkey), None)
+        } else {
+            let password = if let Some(password) = password {
+                Some(password)
+            } else if self.plugin_mgr.keystore_require_password() {
+                Some(read_password(false, None)?)
+            } else {
+                None
+            };
+            (
+                AddressPayload::from_pubkey_hash(from_account.unwrap()),
+                password,
+            )
+        };
+        let from_address = Address::new(network_type, from_address_payload.clone());
+
+        if let Some(from_locked_address) = from_locked_address.as_ref() {
+            let args = from_locked_address.payload().args();
+            let err_prefix = "Invalid from-locked-address's args";
+            if args.len() != 28 {
+                return Err(format!("{}: invalid {}", err_prefix, args.len()));
+            }
+            let mut since_bytes = [0u8; 8];
+            since_bytes.copy_from_slice(&args[20..]);
+            let since = Since::from_raw_value(u64::from_le_bytes(since_bytes));
+            if !since.flags_is_valid() {
+                return Err(format!("{}: invalid since flags", err_prefix));
+            }
+            if !since.is_absolute() {
+                return Err(format!("{}: only support absolute since value", err_prefix));
+            }
+            if since.extract_metric().map(|(ty, _)| ty) != Some(SinceType::EpochNumberWithFraction)
+            {
+                return Err(format!("{}: only support epoch since value", err_prefix));
+            }
+        }
+
+        let genesis_info = self.genesis_info()?;
+
+        // For check index database is ready
+        self.with_db(|_| ())?;
+        let index_dir = self.index_dir.clone();
+        let genesis_hash = genesis_info.header().hash();
+        let genesis_info_clone = genesis_info.clone();
+
+        // The lock hashes for search live cells
+        let mut lock_hashes = vec![Script::from(&from_address_payload).calc_script_hash()];
+        let mut helper = TxHelper::default();
+
+        let from_lock_arg = H160::from_slice(from_address.payload().args().as_ref()).unwrap();
+        let mut path_map: HashMap<H160, DerivationPath> = Default::default();
+        let (change_address_payload, change_path) =
+            if let Some(last_change_address) = last_change_address_opt {
+                // Behave like HD wallet
+                let change_last =
+                    H160::from_slice(last_change_address.payload().args().as_ref()).unwrap();
+                let key_set = self.plugin_mgr.keystore_handler().derived_key_set(
+                    from_lock_arg.clone(),
+                    receiving_address_length,
+                    change_last.clone(),
+                    DERIVE_CHANGE_ADDRESS_MAX_LEN,
+                    None,
+                )?;
+                let mut change_path_opt = None;
+                for (path, hash160) in key_set.external.iter().chain(key_set.change.iter()) {
+                    if hash160 == &change_last {
+                        change_path_opt = Some(path.clone());
+                    }
+                    path_map.insert(hash160.clone(), path.clone());
+                    let payload = AddressPayload::from_pubkey_hash(hash160.clone());
+                    lock_hashes.push(Script::from(&payload).calc_script_hash());
+                }
+                (
+                    last_change_address.payload().clone(),
+                    change_path_opt.expect("change path not exists"),
+                )
+            } else {
+                (from_address.payload().clone(), DerivationPath::empty())
+            };
+
+        if let Some(from_locked_address) = from_locked_address.as_ref() {
+            lock_hashes.insert(
+                0,
+                Script::from(from_locked_address.payload()).calc_script_hash(),
+            );
+            for lock_arg in std::iter::once(&from_lock_arg).chain(path_map.keys()) {
+                let mut sighash_addresses = Vec::default();
+                sighash_addresses.push(AddressPayload::from_pubkey_hash(lock_arg.clone()));
+                let require_first_n = 0;
+                let threshold = 1;
+                let cfg = MultisigConfig::new_with(sighash_addresses, require_first_n, threshold)?;
+                if cfg.hash160().as_bytes() == &from_locked_address.payload().args()[0..20] {
+                    helper.add_multisig_config(cfg);
+                    break;
+                }
+            }
+            if helper.multisig_configs().is_empty() {
+                return Err(String::from(
+                    "from-locked-address is not created from the key or derived keys",
+                ));
+            }
+        }
+
+        if self.wait_for_sync {
+            sync_to_tip(&self.index_controller)?;
+        }
+
+        let rpc_url = self.rpc_client.url().to_string();
+        let keystore = self.plugin_mgr.keystore_handler();
+        let mut live_cell_cache: HashMap<(OutPoint, bool), (CellOutput, Bytes)> =
+            Default::default();
+        let mut get_live_cell_fn = |out_point: OutPoint, with_data: bool| {
+            get_live_cell_with_cache(&mut live_cell_cache, self.rpc_client, out_point, with_data)
+                .map(|(output, _)| output)
+        };
+
+        let original_tx_hash = FixedHashParser::<H256>::default().parse(original_tx_hash)?;
+        let original_tx_outpoint = OutPoint {
+            original_tx_hash,
+            original_tx_output_index,
+        };
+
+        /// add input to_CKB_cell
+        helper.add_input(
+            original_tx_outpoint,
+            None,
+            &mut get_live_cell_fn,
+            &genesis_info,
+            skip_check,
+        )?;
+
+        let ckb_cell_data = get_live_cell_with_cache(
+            &mut live_cell_cache,
+            self.rpc_client,
+            deposit_request_outpoint,
+            true,
+        )
+        .map(|(_, data)| data)?;
+        let from_ckb_cell_data = ToCKBCellData::from_slice(ckb_cell_data.as_slice());
+        let to_capacity = from_ckb_cell_data.lot_size() * 25_000_000 / price * 100_000_000;
+
+        ///check from to capacity
+        let max_mature_number = get_max_mature_number(self.rpc_client)?;
+        let mut from_capacity = 0;
+        let mut infos: Vec<LiveCellInfo> = Default::default();
+        let mut terminator = |_, info: &LiveCellInfo| {
+            if from_capacity >= to_capacity + tx_fee {
+                (true, false)
+            } else if info.type_hashes.is_none()
+                && info.data_bytes == 0
+                && is_mature(info, max_mature_number)
+            {
+                from_capacity += info.capacity;
+                infos.push(info.clone());
+                (from_capacity >= to_capacity + tx_fee, false)
+            } else {
+                (false, false)
+            }
+        };
+
+        if let Err(err) = with_index_db(&index_dir, genesis_hash.unpack(), |backend, cf| {
+            IndexDatabase::from_db(backend, cf, network_type, genesis_info_clone, false)
+                .map(|db| {
+                    for lock_hash in lock_hashes {
+                        db.get_live_cells_by_lock(lock_hash, None, &mut terminator);
+                    }
+                })
+                .map_err(Into::into)
+        }) {
+            return Err(format!(
+                "Index database may not ready, sync process: {}, error: {}",
+                self.index_controller.state().read().to_string(),
+                err.to_string(),
+            ));
+        }
+
+        if tx_fee > ONE_CKB {
+            return Err("Transaction fee can not be more than 1.0 CKB".to_string());
+        }
+        if to_capacity + tx_fee > from_capacity {
+            return Err(format!(
+                "Capacity(mature) not enough: {} => {}",
+                from_address, from_capacity,
+            ));
+        }
+
+        let rest_capacity = from_capacity - to_capacity - tx_fee;
+        if rest_capacity < MIN_SECP_CELL_CAPACITY && tx_fee + rest_capacity > ONE_CKB {
+            return Err("Transaction fee can not be more than 1.0 CKB, please change to-capacity value to adjust".to_string());
+        }
+
+        for info in &infos {
+            helper.add_input(
+                info.out_point(),
+                None,
+                &mut get_live_cell_fn,
+                &genesis_info,
+                skip_check,
+            )?;
+        }
+
+        let lockscript_out_point = OutPoint::new_builder()
+            .tx_hash(
+                Byte32::from_slice(&hex::decode(settings.lockscript.outpoint.tx_hash).unwrap())
+                    .unwrap(),
+            )
+            .index(settings.lockscript.outpoint.index.pack())
+            .build();
+        let typescript_out_point = OutPoint::new_builder()
+            .tx_hash(
+                Byte32::from_slice(&hex::decode(settings.typescript.outpoint.tx_hash).unwrap())
+                    .unwrap(),
+            )
+            .index(settings.typescript.outpoint.index.pack())
+            .build();
+        let typescript_cell_dep = CellDep::new_builder()
+            .out_point(typescript_out_point)
+            .dep_type(DepType::Code.into())
+            .build();
+        let lockscript_cell_dep = CellDep::new_builder()
+            .out_point(lockscript_out_point)
+            .dep_type(DepType::Code.into())
+            .build();
+        helper.transaction = helper
+            .transaction
+            .as_advanced_builder()
+            .cell_dep(typescript_cell_dep)
+            .cell_dep(lockscript_cell_dep)
+            .build();
+        let tockb_data = ToCKBCellData::new_builder()
+            .status(Byte::new(tockb_cell::ToCKBStatus::Initial.int_value()))
+            .lot_size(Byte::new(lot_size))
+            .build()
+            .as_bytes();
+        check_capacity(to_capacity, tockb_data.len())?;
+        let lockscript_hash =
+            hex::decode(settings.lockscript.code_hash).expect("wrong lockscript code hash config");
+        let typescript_hash =
+            hex::decode(settings.typescript.code_hash).expect("wrong typescript code hash config");
+        let typescript = Script::new_builder()
+            .code_hash(Byte32::from_slice(&typescript_hash).unwrap())
+            .hash_type(DepType::Code.into())
+            .args(vec![kind].pack())
+            .build();
+        let lockscript = Script::new_builder()
+            .code_hash(Byte32::from_slice(&lockscript_hash).unwrap())
+            .hash_type(DepType::Code.into())
+            .args(typescript_hash.pack())
+            .build();
+        let to_output = CellOutput::new_builder()
+            .capacity(Capacity::shannons(to_capacity).pack())
+            .type_(Some(typescript).pack())
+            .lock(lockscript)
+            .build();
+        helper.add_output(to_output, tockb_data);
+        if rest_capacity >= MIN_SECP_CELL_CAPACITY {
+            let change_output = CellOutput::new_builder()
+                .capacity(Capacity::shannons(rest_capacity).pack())
+                .lock((&change_address_payload).into())
+                .build();
+            helper.add_output(change_output, Bytes::default());
+        }
+
+        let signer = if let Some(from_privkey) = from_privkey {
+            get_privkey_signer(from_privkey)
+        } else {
+            let new_client = HttpRpcClient::new(rpc_url);
+            get_keystore_signer(
+                keystore,
+                new_client,
+                change_path,
+                path_map,
+                from_lock_arg,
+                password,
+            )
+        };
+        for (lock_arg, signature) in
+            helper.sign_inputs(signer, &mut get_live_cell_fn, skip_check)?
+        {
+            helper.add_signature(lock_arg, signature)?;
+        }
+        let tx = helper.build_tx(&mut get_live_cell_fn, skip_check)?;
+        let tx_hash = self
+            .rpc_client
+            .send_transaction(tx.data())
+            .map_err(|err| format!("Send transaction error: {}", err))?;
+        assert_eq!(tx.hash(), tx_hash.pack());
+        Ok(tx)
+    }
+
     fn supply_capacity(
         &mut self,
         helper: &mut TxHelper,
@@ -721,6 +1093,42 @@ impl<'a> CliSubCommand for ToCkbSubCommand<'a> {
                     Ok(Output::new_output(tx_hash))
                 }
             }
+            ("bonding", Some(m)) => {
+                let settings = Settings::new(&config_path).map_err(|e| {
+                    format!("failed to load config from {}, err: {}", &config_path, e)
+                })?;
+                let args = BondingArgs {
+                    original_tx_hash: get_arg_value(m, "original_tx_hash")?
+                        .parse()
+                        .map_err(|_e| "parse original_tx_hash error".to_owned())?,
+                    original_tx_output_index: get_arg_value(m, "original_tx_output_index")?
+                        .parse()
+                        .map_err(|_e| "parse original_tx_output_index error".to_owned())?,
+                    kind: get_arg_value(m, "kind")?
+                        .parse()
+                        .map_err(|_e| "parse kind error".to_owned())?,
+                    privkey_path: m.value_of("privkey-path").map(|s| s.to_string()),
+                    from_account: m.value_of("from-account").map(|s| s.to_string()),
+                    from_locked_address: m.value_of("from-locked-address").map(|s| s.to_string()),
+                    password: None,
+                    tx_fee: get_arg_value(m, "tx-fee")?,
+                    derive_receiving_address_length: Some(get_arg_value(
+                        m,
+                        "derive-receiving-address-length",
+                    )?),
+                    derive_change_address: m
+                        .value_of("derive-change-address")
+                        .map(|s| s.to_string()),
+                };
+                let tx = self.bonding(args, false, settings)?;
+                if debug {
+                    let rpc_tx_view = json_types::TransactionView::from(tx);
+                    Ok(Output::new_output(rpc_tx_view))
+                } else {
+                    let tx_hash: H256 = tx.hash().unpack();
+                    Ok(Output::new_output(tx_hash))
+                }
+            }
             _ => Err(Self::subcommand().generate_usage()),
         }
     }
@@ -814,6 +1222,21 @@ pub struct DepositRequestArgs {
     pub pledge: u64,
     pub lot_size: u8,
     pub kind: u8,
+}
+
+#[derive(Clone, Debug)]
+pub struct BondingArgs {
+    pub privkey_path: Option<String>,
+    pub from_account: Option<String>,
+    pub from_locked_address: Option<String>,
+    pub password: Option<String>,
+    pub derive_receiving_address_length: Option<String>,
+    pub derive_change_address: Option<String>,
+    pub tx_fee: String,
+
+    pub kind: u8,
+    pub original_tx_hash: String,
+    pub original_tx_output_index: u8,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
